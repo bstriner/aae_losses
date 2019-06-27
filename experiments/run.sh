1fd13d56db090f1b9829d521cf959814bea60ec4
Ben <bstriner@gmail.com>
2019-06-26 21:09:32 -0400

    typo

--- experiments/run.sh
+++ experiments/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-export PYTHONPATH=/data/VOL3/bstriner/aae_loses:$PYTHONPATH
+export PYTHONPATH=/data/VOL3/bstriner/aae_losses:$PYTHONPATH
 export TF_XLA_FLAGS=--tf_xla_cpu_global_jit
-cd /data/VOL3/bstriner/aae_loses/experiments
+cd /data/VOL3/bstriner/aae_losses/experiments
 OUTPUT_FILE="$1"
 shift
 python3 train.py "$@" > "${OUTPUT_FILE}" 2>&1
